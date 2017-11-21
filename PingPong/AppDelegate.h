b5fbecf178d62d24183f899d327571b21a6b6897
Maria <mari.semakova.a@gmail.com>
2017-11-21 20:28:02 +0300

    Initial Commit

---
+++ PingPong/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PingPong
+//
+//  Created by Maria Semakova on 11/21/17.
+//  Copyright © 2017 Maria Semakova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
